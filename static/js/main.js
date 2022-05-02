e722a79074a302b60c8b517e0b864a0528b00c11
R0-M4N <fannroman850@gmail.com>
2022-05-02 17:23:24 +0200

    create board not done yet

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,10 @@
 import {boardsManager} from "./controller/boardsManager.js";
+import {anotherButton, showInputField} from "./getUserInput.js";
 
 function init() {
     boardsManager.loadBoards();
 }
 
 init();
+
+anotherButton.addEventListener("click", showInputField);
