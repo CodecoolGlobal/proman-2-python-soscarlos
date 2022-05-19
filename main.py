0e4a9b722799eab21a62f19d7e9853a865ada813
Carlos <soscarlos@gmail.com>
2022-05-19 15:25:12 +0200

    routes changed for heroku deployment

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ connection = psycopg2.connect(connection_string)
 
 mimetypes.add_type('application/javascript', '.js')
 app = Flask(__name__)
-app.config["SERVER_NAME"] = 'localhost.localdomain:5000'
+# app.config["SERVER_NAME"] = 'localhost.localdomain:5000'
 load_dotenv()
 app.secret_key = "Super Duper Secret Key"
 
--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,4 +1,4 @@
-from __main__ import app
+from main import app
 from flask import render_template, request, redirect, session
 
 import mimetypes
